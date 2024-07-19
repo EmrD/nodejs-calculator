ef72e986b2a333761e11a33f8410cf31e61aa7d2
Emr <127445447+EmrD@users.noreply.github.com>
2024-07-20 00:19:19 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const server = express();
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("Welcome to calculator RestAPI.    Usage: /plus/:num1/:num2 or /minus/:num1/:num2");
+});
+
+server.get("/plus/:num1/:num2", (req, res) => {
+  const { num1, num2 } = req.params;
+  const result = parseInt(num1) + parseInt(num2);
+  
+  if (!isNaN(result)) {
+    res.status(200).send(`Result: ${result}`);
+  } else {
+    res.status(400).send("Please enter valid numbers");
+  }
+});
+
+server.get("/minus/:num1/:num2", (req, res) => {
+  const { num1, num2 } = req.params;
+  const result = parseInt(num1) - parseInt(num2);
+  
+  if (!isNaN(result)) {
+    res.status(200).send(`Result: ${result}`);
+  } else {
+    res.status(400).send("Please enter valid numbers");
+  }
+});
+
+server.listen(5000, () => {
+  console.log("Listening on http://localhost:5000");
+});
