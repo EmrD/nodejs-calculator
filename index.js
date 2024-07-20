465553d586c0f1806dd119b95433ca6554e231d7
Emr <127445447+EmrD@users.noreply.github.com>
2024-07-20 12:22:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 
 const server = express();
+
 server.use(express.json());
+server.use(cors({ origin: true }));
 
 server.get("/", (req, res) => {
-  res.send("Welcome to calculator RestAPI.    Usage: /plus/num1/num2 or /minus/num1/num2");
+  res.send("Welcome to calculator RestAPI. Usage: /plus/:num1/:num2 or /minus/:num1/:num2");
 });
 
 server.get("/plus/:num1/:num2", (req, res) => {
   const { num1, num2 } = req.params;
   const result = parseInt(num1) + parseInt(num2);
-  
+
   if (!isNaN(result)) {
-    res.status(200).send(`Result: ${result}`);
+    res.status(200).json({ result });
   } else {
     res.status(400).send("Please enter valid numbers");
   }
@@ -21,14 +24,16 @@ server.get("/plus/:num1/:num2", (req, res) => {
 server.get("/minus/:num1/:num2", (req, res) => {
   const { num1, num2 } = req.params;
   const result = parseInt(num1) - parseInt(num2);
-  
+
   if (!isNaN(result)) {
-    res.status(200).send(`Result: ${result}`);
+    res.status(200).json({ result });
   } else {
     res.status(400).send("Please enter valid numbers");
   }
 });
 
-server.listen(4000, () => {
-  console.log("Listening on http://localhost:4000");
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
 });
